20042b2bc591566fc40890f4cafa4d7b8beb4ca6
unknown <andres@zerocode.la>
2026-07-10 21:12:50 -0500

    updates9

--- src/assets/icons/visitas/index.js
+++ src/assets/icons/visitas/index.js
@@ -1,11 +1,11 @@
-import amigos from './amigos.png';
-import temporal from './temporal.png';
-import permanente from './permanente.png';
-import huespedTemporal from './huesped-temporal.png';
+import amigosFamiliares from './finales/amigos_familiares.png';
+import temporal from './finales/temporal.png';
+import permanente from './finales/permanente.png';
+import huespedTemporal from './finales/huesped_temporal.png';
 import visitavalida from './visitavalida.png';
 import visitanovalida from './visitanovalida.png';
 
-const tipoVisitaIcons = { amigos, temporal, permanente, 'huesped-temporal': huespedTemporal };
+const tipoVisitaIcons = { amigos: amigosFamiliares, temporal, permanente, 'huesped-temporal': huespedTemporal };
 
 export default tipoVisitaIcons;
 export { visitavalida, visitanovalida };
